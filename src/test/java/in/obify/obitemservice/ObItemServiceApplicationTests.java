983b72beb235a7a382e9e525dac5638b62267b23
nadineboukadida <78963245+nadineboukadida@users.noreply.github.com>
2023-11-30 13:32:39 +0100

    added tests

--- src/test/java/in/obify/obitemservice/ObItemServiceApplicationTests.java
+++ src/test/java/in/obify/obitemservice/ObItemServiceApplicationTests.java
@@ -1,13 +1,42 @@
 package in.obify.obitemservice;
 
+import in.obify.obitemservice.controller.ItemCategoryController;
+import in.obify.obitemservice.model.ItemCategoryModel;
+import in.obify.obitemservice.repository.ItemCategoryRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 @SpringBootTest
-class ObItemServiceApplicationTests {
+class ItemCategoryControllerTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @InjectMocks
+    private ItemCategoryController itemCategoryController;
 
+    @Mock
+    private ItemCategoryRepository itemCategoryRepository;
+
+    @Test
+    void getAllItemCategory() {
+        // Given
+        ItemCategoryModel category1 = new ItemCategoryModel("1", "Category 1");
+        ItemCategoryModel category2 = new ItemCategoryModel("2", "Category 2");
+
+        List<ItemCategoryModel> categories = Arrays.asList(category1, category2);
+
+        when(itemCategoryRepository.findAll()).thenReturn(categories);
+
+        // When
+        List<ItemCategoryModel> result = itemCategoryController.getAllItemCategory();
+
+        // Then
+        assertEquals(categories, result);
+    }
 }
